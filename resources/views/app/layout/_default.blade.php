f11c0a48325c0e5b5c0f96d47f7073d12bbc7efd
Sergio Serra <sergioserra.2003@gmail.com>
2023-11-17 18:51:02 +0000

    Layouts

--- resources/views/app/layout/_default.blade.php
+++ resources/views/app/layout/_default.blade.php
@@ -11,7 +11,7 @@
             <div class="app-main flex-column flex-row-fluid " id="kt_app_main">
                 <!--begin::Content wrapper-->
                 <div class="d-flex flex-column flex-column-fluid">
-@include('app.layout.partials._content')
+@yield('content')
 
                                     </div>
                 <!--end::Content wrapper-->
---
+++ resources/views/app/profile.blade.php
@@ -0,0 +1,7 @@
+@extends('app.index')
+
+@section('title', 'Profile')
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
