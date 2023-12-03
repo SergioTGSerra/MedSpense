4a3287068467c75240c8311c3c2c0fd4f501a591
Sérgio Serra <sergioserra.2003@gmail.com>
2023-12-03 18:31:45 +0000

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        $doctorUser = \App\Models\User::create([
             'user_id' => Str::uuid(),
             'name' => 'Sergio Serra',
-            'email' => 'sergioserra.2003@gmail.com',
+            'email' => 'sergioserra@ipvc.pt',
             'nif' => fake()->unique()->randomNumber(9),
             'email_verified_at' => now(),
             'password' => bcrypt('password'),
-            'user_type' => 'Admin',
+            'user_type' => 'Doctor',
             'birth_date' => '1990-01-01',
             'gender' => 'Male',
             'phone_number' => '123-456-7890',
@@ -30,5 +30,55 @@ class DatabaseSeeder extends Seeder
             'emergency_contact' => 'Jane Doe (123-789-4560)',
             'remember_token' => Str::random(10),
         ]);
+
+        $patientUser = \App\Models\User::create([
+            'user_id' => Str::uuid(),
+            'name' => 'Sergio Tiago',
+            'email' => 'sergioserra.2003@gmail.com',
+            'nif' => fake()->unique()->randomNumber(9),
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'user_type' => 'Patient',
+            'birth_date' => '1990-01-01',
+            'gender' => 'Male',
+            'phone_number' => '123-456-7890',
+            'address' => '123 Main St',
+            'emergency_contact' => 'Jane Doe (123-789-4560)',
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\Doctor::create([
+            'doctor_id' => $doctorUser->user_id,
+            'specialty' => 'General Medicine',
+            'admission_date' => '2021-01-01',
+        ]);
+
+        \App\Models\Patient::create([
+            'patient_id' => $patientUser->user_id,
+            'blood_type' => 'A+',
+            'medical_history' => 'No medical history',
+            'insurance_information' => 'No insurance',
+        ]);
+
+        $appointment = \App\Models\Appointment::create([
+            'appointment_id' => Str::uuid(),
+            'doctor_id' => $doctorUser->user_id,
+            'patient_id' => $patientUser->user_id,
+            'diagnosis' => 'No diagnosis',
+            'prescription' => 'No prescription',
+            'notes' => 'No notes',
+            'appointment_date' => '2021-01-01 10:00:00',
+        ]);
+
+        \App\Models\Invoice::create([
+            'invoice_id' => Str::uuid(),
+            'status' => 'Unpaid',
+            'payment_date' => '2021-01-01',
+            'payment_method' => 'Cash',
+            'total' => 10,
+            'patient_id' => $patientUser->user_id,
+            'appointment_id' => $appointment->appointment_id,
+        ]);
+        
     }
 }
