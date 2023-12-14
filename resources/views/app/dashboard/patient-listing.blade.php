ca465be9dee5f46a5e152d7c4043b3a80cb867fb
AAVANDRE10 <andrevale_6@hotmail.com>
2023-12-14 03:25:26 +0000

    add content page patient-listing

---
+++ resources/views/app/dashboard/patient-listing.blade.php
@@ -0,0 +1,247 @@
+@extends('app.layout.index')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="card">
+    <!--begin::Card header-->
+    <div class="card-header border-0 pt-6">
+        <!--begin::Card title-->
+        <div class="card-title">
+            <!--begin::Search-->
+            <div class="d-flex align-items-center position-relative my-1">
+                <i class="ki-outline ki-magnifier fs-3 position-absolute ms-5"></i>
+                <input type="text" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Search user" />
+            </div>
+            <!--end::Search-->
+        </div>
+        <!--begin::Card title-->
+        <!--begin::Card toolbar-->
+        <div class="card-toolbar">
+            <!--begin::Toolbar-->
+            <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
+                <!--begin::Add user-->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_user">
+                <i class="ki-outline ki-plus fs-2"></i>Add Patient</button>
+                <!--end::Add user-->
+            </div>
+            <!--end::Toolbar-->
+            <!--begin::Modal - Add task-->
+            <div class="modal fade" id="kt_modal_add_user" tabindex="-1" aria-hidden="true">
+                <!--begin::Modal dialog-->
+                <div class="modal-dialog modal-dialog-centered mw-650px">
+                    <!--begin::Modal content-->
+                    <div class="modal-content">
+                        <!--begin::Modal header-->
+                        <div class="modal-header" id="kt_modal_add_user_header">
+                            <!--begin::Modal title-->
+                            <h2 class="fw-bold">Add User</h2>
+                            <!--end::Modal title-->
+                            <!--begin::Close-->
+                            <div class="btn btn-icon btn-sm btn-active-icon-primary" data-bs-dismiss="modal">
+                                <i class="ki-outline ki-cross fs-1"></i>
+                            </div>
+                            <!--end::Close-->
+                        </div>
+                        <!--end::Modal header-->
+                        <!--begin::Modal body-->
+                        <div class="modal-body px-5 my-7">
+                            <!--begin::Form-->
+                            <form id="kt_modal_add_user_form" class="form" action="#">
+                                <!--begin::Scroll-->
+                                <div class="d-flex flex-column scroll-y px-5 px-lg-10" id="kt_modal_add_user_scroll" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_add_user_header" data-kt-scroll-wrappers="#kt_modal_add_user_scroll" data-kt-scroll-offset="300px">
+                                    <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="d-block fw-semibold fs-6 mb-5">Avatar</label>
+                                        <!--end::Label-->
+                                        <!--begin::Image placeholder-->
+                                        <style>.image-input-placeholder { background-image: url('assets/media/svg/files/blank-image.svg'); } [data-bs-theme="dark"] .image-input-placeholder { background-image: url('assets/media/svg/files/blank-image-dark.svg'); }</style>
+                                        <!--end::Image placeholder-->
+                                        <!--begin::Image input-->
+                                        <div class="image-input image-input-outline image-input-placeholder" data-kt-image-input="true">
+                                            <!--begin::Preview existing avatar-->
+                                            <div class="image-input-wrapper w-125px h-125px" style="background-image: url(assets/media/avatars/300-6.jpg);"></div>
+                                            <!--end::Preview existing avatar-->
+                                            <!--begin::Label-->
+                                            <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+                                                <i class="ki-outline ki-pencil fs-7"></i>
+                                                <!--begin::Inputs-->
+                                                <input type="file" name="avatar" accept=".png, .jpg, .jpeg" />
+                                                <input type="hidden" name="avatar_remove" />
+                                                <!--end::Inputs-->
+                                            </label>
+                                            <!--end::Label-->
+                                            <!--begin::Cancel-->
+                                            <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel avatar">
+                                                <i class="ki-outline ki-cross fs-2"></i>
+                                            </span>
+                                            <!--end::Cancel-->
+                                            <!--begin::Remove-->
+                                            <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
+                                                <i class="ki-outline ki-cross fs-2"></i>
+                                            </span>
+                                            <!--end::Remove-->
+                                        </div>
+                                        <!--end::Image input-->
+                                        <!--begin::Hint-->
+                                        <div class="form-text">Allowed file types: png, jpg, jpeg.</div>
+                                        <!--end::Hint-->
+                                    </div>
+                                    <!--end::Input group-->
+                                    <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="required fw-semibold fs-6 mb-2">Full Name</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input type="text" name="user_name" class="form-control form-control-solid mb-3 mb-lg-0" placeholder="Full name" value="Emma Smith" />
+                                        <!--end::Input-->
+                                    </div>
+                                    <!--end::Input group-->
+                                    <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="required fw-semibold fs-6 mb-2">Email</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input type="email" name="user_email" class="form-control form-control-solid mb-3 mb-lg-0" placeholder="example@domain.com" value="smith@kpmg.com" />
+                                        <!--end::Input-->
+                                    </div>
+                                     <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="required fw-semibold fs-6 mb-2">NIF</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input type="email" name="user_email" class="form-control form-control-solid mb-3 mb-lg-0" placeholder="xxxxxxxxx" value="547689987" />
+                                        <!--end::Input-->
+                                    </div>
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="fs-6 fw-semibold mb-2 required">Date of Birth</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input class="form-control form-control-solid" name="calendar_event_start_date" placeholder="Pick a date" id="kt_calendar_datepicker_start_date" />
+                                        <!--end::Input-->
+                                    </div>
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="fs-6 fw-semibold mb-2 required">Gender</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <select name="language" aria-label="Select a Language" data-control="select2" data-placeholder="Select a language..." class="form-select form-select-solid form-select-lg">
+                                            <option value="">Select a Language...</option>
+                                            <option value="id">Male</option>
+                                            <option value="msa">Female</option>
+                                            <option value="msa">Not Defined</option>
+                                        </select>
+                                        <!--end::Input-->
+                                    </div>
+                                    <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="required fw-semibold fs-6 mb-2">Address</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input type="address" name="address" class="form-control form-control-solid mb-3 mb-lg-0" placeholder="xxxxxxxxx" value="Rua dos Combatentes" />
+                                        <!--end::Input-->
+                                    </div>
+                                    <!--end::Input group-->
+                                    <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="required fw-semibold fs-6 mb-2">Cell Phone</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input type="address" name="cellphone" class="form-control form-control-solid mb-3 mb-lg-0" placeholder="xxxxxxxxx" value="877564654" />
+                                        <!--end::Input-->
+                                    </div>
+                                    <!--end::Input group-->
+                                    <!--begin::Input group-->
+                                    <div class="fv-row mb-7">
+                                        <!--begin::Label-->
+                                        <label class="required fw-semibold fs-6 mb-2">Emergency Cell Phone</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <input type="address" name="cellphone emergency" class="form-control form-control-solid mb-3 mb-lg-0" placeholder="xxxxxxxxx" value="877564654" />
+                                        <!--end::Input-->
+                                    </div>
+                                    <!--end::Input group-->
+                                <!--begin::Actions-->
+                                <div class="text-center pt-10">
+                                    <button type="reset" class="btn btn-light me-3" data-kt-users-modal-action="cancel">Discard</button>
+                                    <button type="submit" class="btn btn-primary" data-kt-users-modal-action="submit">
+                                        <span class="indicator-label">Add Patient</span>
+                                        <span class="indicator-progress">Please wait... 
+                                        <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                    </button>
+                                </div>
+                                <!--end::Actions-->
+                            </form>
+                            <!--end::Form-->
+                        </div>
+                        <!--end::Modal body-->
+                    </div>
+                    <!--end::Modal content-->
+                </div>
+                <!--end::Modal dialog-->
+            </div>
+            <!--end::Modal - Add task-->
+        </div>
+        <!--end::Card toolbar-->
+    </div>
+    <!--end::Card header-->
+    <!--begin::Card body-->
+    <div class="card-body py-4">
+        <!--begin::Table-->
+        <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
+            <thead>
+                <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                    <th>
+                        <th class="min-w-125px">Name</th>
+                        <th class="min-w-125px">NIF</th>
+                        <th class="min-w-125px">Date of birth</th>
+                        <th class="min-w-125px">Cell Phone</th>
+                        <th class="min-w-125px">Emergency Cell Phone</th>
+                        <th class="min-w-125px">Address</th>
+                        <th class="min-w-125px">Gender</th>
+                        <th class="min-w-125px">E-mail</th>
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="text-gray-600 fw-semibold">
+                <tr>
+                    <td>
+                        <td class="d-flex align-items-center">
+                            <!--begin:: Avatar -->
+                            <div class="symbol symbol-circle symbol-50px overflow-hidden me-3">
+                                <a href="apps/user-management/users/view.html">
+                                    <div class="symbol-label">
+                                        <img src="assets/media/avatars/300-6.jpg" alt="Emma Smith" class="w-100" />
+                                    </div>
+                                </a>
+                            </div>
+                            <!--end::Avatar-->
+                            <!--begin::User details-->
+                            <div class="d-flex flex-column">
+                                <a href="apps/user-management/users/view.html" class="text-gray-800 text-hover-primary mb-1">Emma Smith</a>
+                                <span>smith@kpmg.com</span>
+                            </div>
+                            <!--begin::User details-->
+                        </td>
+                    </td>
+                    <td>789586214</td>
+                    <td>30/08/2000</td>
+                    <td>925789545</td>
+                    <td>964189545</td>
+                    <td>Rua do Combatentes Nº13 4740-409</td>
+                    <td>Female</td>
+                    <td>emfig@medspense.pt</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
